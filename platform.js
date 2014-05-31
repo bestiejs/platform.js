1a0f11543de49b12135a3167c9f60db1f668cec6
Benjamin Tan <demoneaux@gmail.com>
2014-05-31 10:21:35 +0800

    Document common values of `platform.os.family`
    Closes #55.
    
    [ci skip]

--- platform.js
+++ platform.js
@@ -1015,6 +1015,11 @@
       /**
        * The family of the OS.
        *
+       * Common values include:
+       * "Windows", "Windows Server 2008 R2 / 7", "Windows Server 2008 / Vista",
+       * "Windows XP", "OS X", "Ubuntu", "Debian", "Fedora", "Red Hat", "SuSE",
+       * "Android", "iOS" and "Windows Phone OS"
+       *
        * @memberOf platform.os
        * @type string|null
        */
