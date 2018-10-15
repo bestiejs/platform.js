8790427f13e691d6a7ab3a2d7ffdbc1b7c58c278
Giuseppe <giutrotta@gmail.com>
2018-10-15 11:06:19 +0200

    Updated banner in bump.js

--- bump/bump.js
+++ bump/bump.js
@@ -2,7 +2,7 @@ var replace = require("replace");
 var version = process.argv.slice(2);
 
 var banner = `/*!
- * Platform.js
+ * Platform.js v<VERSION>
  * Copyright 2014-2018 Benjamin Tan
  * Copyright 2011-2013 John-David Dalton
  * Available under MIT license
