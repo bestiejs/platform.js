385e3fc97df55466899e004e5115caa9b7f06a42
Benjamin Tan <demoneaux@gmail.com>
2020-06-24 00:17:35 +0800

    Add "Linux" to list of common OS family values.
    Closes #177.

--- platform.js
+++ platform.js
@@ -1143,8 +1143,8 @@
        *
        * Common values include:
        * "Windows", "Windows Server 2008 R2 / 7", "Windows Server 2008 / Vista",
-       * "Windows XP", "OS X", "Ubuntu", "Debian", "Fedora", "Red Hat", "SuSE",
-       * "Android", "iOS" and "Windows Phone"
+       * "Windows XP", "OS X", "Linux", "Ubuntu", "Debian", "Fedora", "Red Hat",
+       * "SuSE", "Android", "iOS" and "Windows Phone"
        *
        * @memberOf platform.os
        * @type string|null
